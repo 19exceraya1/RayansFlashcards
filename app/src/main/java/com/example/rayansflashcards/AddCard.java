c9f1b40036e33cc5a6ce834010e240ee9a640612
19exceraya1 <19exceraya1@gmail.com>
2021-03-20 01:59:34 -0400

    Optional tasks from week 4

--- app/src/main/java/com/example/rayansflashcards/AddCard.java
+++ app/src/main/java/com/example/rayansflashcards/AddCard.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,27 +17,38 @@ public class AddCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_page);
+
         EditText questionInput = findViewById(R.id.questionInput);
-        questionInput.setText(getIntent().getStringExtra("userQuestion"));
         EditText answerInput = findViewById(R.id.answerInput);
-        answerInput.setText(getIntent().getStringExtra("userAnswer"));
+        EditText wrongOption1 = findViewById(R.id.wrongOption1);
+        EditText wrongOption2 = findViewById(R.id.wrongOption2);
         ImageView cancelIcon = findViewById(R.id.cancel_button);
         ImageView saveIcon = findViewById(R.id.save_icon);
+
+        questionInput.setText(getIntent().getStringExtra("userQuestion"));
+        answerInput.setText(getIntent().getStringExtra("userAnswer"));
+
 
 
         saveIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (questionInput.getText().toString().isEmpty() || answerInput.getText().toString().isEmpty() ){
+
+                String returningUserQuestion =  questionInput.getText().toString();
+                String returningUserAnswer = answerInput.getText().toString();
+
+                if(questionInput.getText().toString().trim().length() == 0 || answerInput.getText().toString().trim().length() == 0 || wrongOption1.getText().toString().trim().length() == 0 || wrongOption2.getText().toString().trim().length() == 0){
+                    Toast.makeText(getApplicationContext(), "Question and Answer cannot be blank", Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent data = new Intent(); // create a new Intent, this is where we will put our data
+                    data.putExtra("returningUserQuestion", returningUserQuestion); // puts one string into the Intent, with the key as 'string1'
+                    data.putExtra("returningUserAnswer", returningUserAnswer); // puts another string into the Intent, with the key as 'string2
+                    data.putExtra("wrongOption1", wrongOption1.getText().toString()); // puts one string into the Intent, with the key as 'string1'
+                    data.putExtra("wrongOption2", wrongOption2.getText().toString());
+                    setResult(RESULT_OK, data); // set result code and bundle data for response
+                    finish(); // closes this activity and pass data to the original activity that launched this activity
 
                 }
-                returningUserQuestion = ((EditText)findViewById(R.id.questionInput)).getText().toString();
-                returningUserAnswer = ((EditText) findViewById(R.id.answerInput)).getText().toString();
-                Intent data = new Intent(); // create a new Intent, this is where we will put our data
-                data.putExtra("userQuestion", returningUserQuestion); // puts one string into the Intent, with the key as 'string1'
-                data.putExtra("userAnswer", returningUserAnswer); // puts another string into the Intent, with the key as 'string2
-                setResult(RESULT_OK, data); // set result code and bundle data for response
-                finish(); // closes this activity and pass data to the original activity that launched this activity
             }
         });
 
--- app/src/main/java/com/example/rayansflashcards/MainActivity.java
+++ app/src/main/java/com/example/rayansflashcards/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,19 +129,48 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        setContentView(R.layout.activity_main_2);
-        String userQuestion = "";
-        String userAnswer = "";
+
+        Snackbar.make(findViewById(R.id.flashcard_question),
+                "Card successfully created",
+                Snackbar.LENGTH_SHORT)
+                .show();
+
+        super.onActivityResult(requestCode, resultCode, data);
+        setContentView(R.layout.activity_main);
+        String userQuestion;
+        String userAnswer;
+        String wrongOption1;
+        String wrongOption2;
+
         if (requestCode == 100 && resultCode == RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
-             userQuestion = data.getExtras().getString("userQuestion"); // 'string1' needs to match the key we used when we put the string in the Intent
-             userAnswer = data.getExtras().getString("userAnswer");
+            userQuestion = data.getExtras().getString("returningUserQuestion");
+            userAnswer = data.getExtras().getString("returningUserAnswer");
+            wrongOption1 = data.getExtras().getString("wrongOption1");
+            wrongOption2 = data.getExtras().getString("wrongOption2");
+        } else {
+            userQuestion = "";
+            userAnswer = "";
+            wrongOption1 = "";
+            wrongOption2 = "";
         }
+
         TextView questionTextView = findViewById(R.id.flashcard_question);
         TextView answerTextView = findViewById(R.id.flashcard_answer);
+        TextView answer1 = findViewById(R.id.Answer1);
+        TextView answer2 = findViewById(R.id.Answer2);
+        TextView answer3 = findViewById(R.id.Answer3);
+        TextView reset = findViewById(R.id.reset);
+        ImageView invisible = findViewById(R.id.invisible_button);
+        ImageView visible = findViewById(R.id.visible_button);
         ImageView newPageIcon = findViewById(R.id.newpage);
+        ImageView editIcon= findViewById(R.id.edit);
 
         questionTextView.setText(userQuestion);
         answerTextView.setText(userAnswer);
+        answer1.setText(wrongOption1);
+        answer2.setText(wrongOption2);
+        answer3.setText(userAnswer);
+
 
         questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,12 +189,80 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        newPageIcon.setOnClickListener(new View.OnClickListener() {
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer1.setBackgroundColor(Color.parseColor("#e62200"));
+            }
+        });
+        answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer3.setBackgroundColor(Color.parseColor("#66f542"));
+                answer2.setBackgroundColor(Color.parseColor("#e62200"));
+
+            }
+        });
+        answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer3.setBackgroundColor(Color.parseColor("#66f542"));
+
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer1.setBackgroundColor(Color.parseColor("#AEFFF29A"));
+                answer2.setBackgroundColor(Color.parseColor("#AEFFF29A"));
+                answer3.setBackgroundColor(Color.parseColor("#AEFFF29A"));
+
+            }
+
+        });
+
+        invisible.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                answer1.setVisibility(View.INVISIBLE);
+                answer2.setVisibility(View.INVISIBLE);
+                answer3.setVisibility(View.INVISIBLE);
+                invisible.setVisibility(View.INVISIBLE);
+                visible.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        visible.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                answer1.setVisibility(View.VISIBLE);
+                answer2.setVisibility(View.VISIBLE);
+                answer3.setVisibility(View.VISIBLE);
+                invisible.setVisibility(View.VISIBLE);
+                visible.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+        editIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCard.class);
+                intent.putExtra("userQuestion", questionTextView.getText());
+                intent.putExtra("userAnswer", answerTextView.getText());
                 MainActivity.this.startActivityForResult(intent, 100);
             }
         });
-    }
+
+        newPageIcon.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick (View v){
+                Intent intent = new Intent(MainActivity.this, AddCard.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+            });
+        }
 }
